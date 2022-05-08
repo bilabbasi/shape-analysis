a105dc903fc849db8435bc642751e1d38edbd1f0
Bilal Abbasi <bilalabbasi@192.168.1.126>
2022-05-08 14:56:43 +0100

    WIP: adds pooling layers and an autoencoder

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
-from .architectures import LinearMetricNet, MetricConvNet, MetricResNet
+from .architectures import (AutoEncoder, LinearMetricNet, MetricConvNet,
+                            MetricResNet)
+from .mesh_pool import MeshPool
 from .metric import *
 from .metric_conv import MetricConv
